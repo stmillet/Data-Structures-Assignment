238105654a2ae4ab4a320233060e3a15c2cbf20e
Paige Harper <pharper2@byu.edu>
2019-09-27 08:50:53 -0600

    New Code

--- DataStructuresAssignment/Controllers/IndexController.cs
+++ DataStructuresAssignment/Controllers/IndexController.cs
@@ -11,6 +11,22 @@ namespace DataStructuresAssignment.Controllers
         // GET: Index
         public ActionResult Index()
         {
+            Queue<string> myQueue = new Queue<string>();
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+            public static Random random = new Random();
+
+            public static string randomName()
+            {
+                string[] names = new string[8] { "Dan Morain", "Emily Bell", "Carol Roche", "Ann Rose", "John Miller", "Greg Anderson", "Arthur McKinney", "Joann Fisher" };
+                int randomIndex = Convert.ToInt32(random.NextDouble() * 7);
+                return names[randomIndex];
+            }
+
+            public static int randomNumberInRange()
+               {
+                    return Convert.ToInt32(random.NextDouble() * 20);
+                }
             return View();
         }
     }
