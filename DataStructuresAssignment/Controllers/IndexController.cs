563a1afb270c88ad0ee7973dd2104b22a8216507
Emmett Madsen <emmett1013@gmail.com>
2019-09-27 10:04:23 -0600

    comments

--- DataStructuresAssignment/Controllers/IndexController.cs
+++ DataStructuresAssignment/Controllers/IndexController.cs
@@ -38,10 +38,12 @@ namespace DataStructuresAssignment.Controllers
                 theLine.Enqueue(randomName);
             }    
 
+            //Foreach statement that gives each customer in the line the number of burgers they ordered that instance. 
+           
             foreach (string customerName in theLine)
             {
                 int numBurgers = randomNumberInRange();
-                if !(isFound(customerName))
+                if (!isFound(customerName))
                 {
                     customerInfo.Add(customerName, 0);
                 }
