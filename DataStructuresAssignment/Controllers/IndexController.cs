043660c9188d226b94118320d6da5b600ef30859
Spencer Millett <millett.spencer@gmail.com>
2019-09-27 09:50:26 -0600

    Didi noting

--- DataStructuresAssignment/Controllers/IndexController.cs
+++ DataStructuresAssignment/Controllers/IndexController.cs
@@ -32,7 +32,7 @@ namespace DataStructuresAssignment.Controllers
             {
                 string randomName = randomName();
                 theLine.Enqueue(randomName);
-            }
+            }    
 
             return View();
         }
