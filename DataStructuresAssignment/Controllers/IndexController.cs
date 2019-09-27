c207ccd907da7260c5f721f0377f611e68f2a705
Paige Harper <pharper2@byu.edu>
2019-09-27 08:56:50 -0600

    New Code

--- DataStructuresAssignment/Controllers/IndexController.cs
+++ DataStructuresAssignment/Controllers/IndexController.cs
@@ -6,6 +6,19 @@ using System.Web.Mvc;
 
 namespace DataStructuresAssignment.Controllers
 {
+    public static Random random = new Random();
+
+    public static string randomName()
+    {
+        string[] names = new string[8] { "Dan Morain", "Emily Bell", "Carol Roche", "Ann Rose", "John Miller", "Greg Anderson", "Arthur McKinney", "Joann Fisher" };
+        int randomIndex = Convert.ToInt32(random.NextDouble() * 7);
+        return names[randomIndex];
+    }
+
+    public static int randomNumberInRange()
+    {
+        return Convert.ToInt32(random.NextDouble() * 20);
+    }
     public class IndexController : Controller
     {
         // GET: Index
